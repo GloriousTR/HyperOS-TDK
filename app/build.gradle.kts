ba4103c0ec0d4c5a5a30f7e0af045de4d713041e
Kadir ÖZTÜRK <87311965+GloriousTR@users.noreply.github.com>
2026-08-21 15:08:54 +0300

    chore: bump HyperTheme reference test to build 30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.glorious.hyperostdk"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29
+        versionCode = 30
         versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
