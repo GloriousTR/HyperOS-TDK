c18bb495ac678da7bcb81ae6ea697a61f7ba4572
Kadir ÖZTÜRK <87311965+GloriousTR@users.noreply.github.com>
2026-08-21 11:33:32 +0300

    chore: bump version to 0.3.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.glorious.hyperostdk"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.3.6"
+        versionCode = 20
+        versionName = "0.3.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
