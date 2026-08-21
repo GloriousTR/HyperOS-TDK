816c0fafedb9299e96a18f098c9eb3582b02374e
Kadir ÖZTÜRK <87311965+GloriousTR@users.noreply.github.com>
2026-08-21 16:58:17 +0300

    chore: bump local JSON schema tracer to build 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.glorious.hyperostdk"
         minSdk = 26
         targetSdk = 36
-        versionCode = 35
+        versionCode = 36
         versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
