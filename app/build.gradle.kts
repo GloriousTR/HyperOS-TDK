68ab71f313178dc6108db3500efa4ad8ae03f7a6
Kadir ÖZTÜRK <87311965+GloriousTR@users.noreply.github.com>
2026-08-21 16:22:46 +0300

    chore: bump frozen metadata test to build 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.glorious.hyperostdk"
         minSdk = 26
         targetSdk = 36
-        versionCode = 34
+        versionCode = 35
         versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
