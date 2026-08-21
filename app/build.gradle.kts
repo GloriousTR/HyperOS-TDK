ee29eb1e6d2dc34da48b88b1a7c7d92d5d1cd4bc
Kadir ÖZTÜRK <87311965+GloriousTR@users.noreply.github.com>
2026-08-21 10:34:26 +0300

    chore: bump HyperOS TDK to v0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.glorious.hyperostdk"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "0.3.1"
+        versionCode = 15
+        versionName = "0.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
