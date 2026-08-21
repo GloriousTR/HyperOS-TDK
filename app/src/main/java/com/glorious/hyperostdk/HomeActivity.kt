f07642e2f637e1082f78267441a0cfe217c410a5
Kadir ÖZTÜRK <87311965+GloriousTR@users.noreply.github.com>
2026-08-21 09:37:44 +0300

    fix: opt in to Material 3 top app bar API

--- app/src/main/java/com/glorious/hyperostdk/HomeActivity.kt
+++ app/src/main/java/com/glorious/hyperostdk/HomeActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -40,6 +41,7 @@ class HomeActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun HomeScreen(
     onThemeTools: () -> Unit,
