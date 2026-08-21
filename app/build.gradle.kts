a41fdc552624c97699be1afb7e9b7bd4a747c5a0
Kadir ÖZTÜRK <87311965+GloriousTR@users.noreply.github.com>
2026-08-21 15:57:47 +0300

    chore: bump build 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.glorious.hyperostdk"
         minSdk = 26
         targetSdk = 36
-        versionCode = 32
+        versionCode = 33
         versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
