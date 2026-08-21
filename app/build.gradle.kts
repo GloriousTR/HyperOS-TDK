a676aaf2c9adbd33a6b7ec87bf840167b35332b0
Kadir ÖZTÜRK <87311965+GloriousTR@users.noreply.github.com>
2026-08-21 14:38:38 +0300

    chore: bump 0.4.1 verification hotfix to build 27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.glorious.hyperostdk"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
+        versionCode = 27
         versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
